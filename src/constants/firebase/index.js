f6f1762d6abe174c2d0137d173053fe124bf472b
Roscoe <roscoevanderboom@protonmail.com>
2020-11-30 12:21:48 +0800

    ammend config.js

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -5,14 +5,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyBd0MJSCCN1nUdzYoCpzYopaU9uCaet-AA",
-  authDomain: "ninjasubs-taipei.firebaseapp.com",
-  databaseURL: "https://ninjasubs-taipei.firebaseio.com",
-  projectId: "ninjasubs-taipei",
-  storageBucket: "ninjasubs-taipei.appspot.com",
-  messagingSenderId: "504240713231",
-  appId: "1:504240713231:web:60f91e943b7bcf79f77d7a",
-  measurementId: "G-KEX87QTP3Z",
+  
 };
 
 export const FieldValue = app.firestore.FieldValue;
